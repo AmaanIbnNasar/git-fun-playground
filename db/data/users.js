41b33bb9bb2241f75c40d6ef6b7692b0ffd4bea0
Oluwadamiloju Makinde <oluwadamilojumakinde@Sharons-MacBook-Pro.local>
2021-11-03 12:52:01 +0000

    Creating database and table

---
+++ db/data/users.js
@@ -0,0 +1,7 @@
+module.exports =[
+    {
+        username: "Abbie", 
+        avatar_img_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftheconversation.com%2Fhow-we-found-coronavirus-in-a-cat-143697&psig=AOvVaw0Av8RsjOyBPQMcSbm7vXoA&ust=1636030102934000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMiWzZud_PMCFQAAAAAdAAAAABAD",
+        online: true
+    }
+]
